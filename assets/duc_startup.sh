868c1cc5e1e5e23bd30913a8faa5d34da4cefe3a
etienne <etienne@amato.top>
2024-10-28 10:50:40 +0100

    index file linking to existing databases

--- assets/duc_startup.sh
+++ assets/duc_startup.sh
@@ -4,14 +4,37 @@
 # if it thinks it is already running.
 rm -rf /run/httpd/*
 
+# Set variable for index.cgi path
+index_file="/var/www/html/index.cgi"
+
+# Check for existing index.cgi and empty it if it exists, ensure it is executable
+if [ -f "$index_file" ]; then
+  > "$index_file"
+  chmod +x "$index_file"
+fi
+
 db_dir="/duc/db/"
+# Start building the index.cgi content
+index_content="#!/bin/sh
+echo 'Content-type: text/html'
+echo ''
+echo '<html><head><title>Available DUC databases</title></head><body>'"
+echo "<ul>" >> "$index_file"
+
 for file in "${db_dir}"*.db; do
 	[ -f "${file}" ] || break
 	filename="$(basename "${file}")"
 	filename="${filename%.*}"
-	echo "#!/bin/sh
-	/usr/local/bin/duc cgi $DUC_CGI_OPTIONS -d ${file}" > /var/www/duc/"${filename}".cgi
-	chmod +x /var/www/duc/"${filename}".cgi
+	# Add a link to the index.cgi, skip if filename is duc or index
+	if [[ "$filename" != "duc" && "$filename" != "index" ]]; then
+		echo "#!/bin/sh
+		/usr/local/bin/duc cgi $DUC_CGI_OPTIONS -d ${file}" > /var/www/duc/"${filename}".cgi
+		chmod +x /var/www/duc/"${filename}".cgi
+		echo "<li><a href=\"/duc/${filename}.cgi\">${filename}</a></li>" >> "$index_file"
+	fi
 done
+
+echo "</ul>" >> "$index_file"
+echo "</body></html>" >> "$index_file"
 
 /usr/sbin/apache2ctl -D FOREGROUND
